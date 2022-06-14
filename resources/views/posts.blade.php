1c7d540fd3fab3af5b06a63619889dc5f2efc492
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-06-14 12:24:18 +0530

    Refactor: Refactor code to get post by its slug.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
         @foreach ($posts as $post)
             <article>
                 <h1>
-                    <a href="/post/{{ $post->id }}">{{ $post->title }}</a>
+                    <a href="/post/{{ $post->slug }}">{{ $post->title }}</a>
                 </h1>
 
                 <p>
