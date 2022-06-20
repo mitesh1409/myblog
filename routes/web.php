d9fc82d866209b1571d866c913a3f21cc8c5fae2
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-06-20 08:10:00 +0530

    Feature: Display all the category posts.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +23,8 @@ Route::get('/posts/{post}', function (Post $post) {
     return view('post', [
         'post' => $post
     ]);
+});
+
+Route::get('/categories/{category}', function (Category $category) {
+    return view('posts', ['posts' => $category->posts]);
 });
